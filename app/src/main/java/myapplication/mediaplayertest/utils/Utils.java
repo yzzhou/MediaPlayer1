c4bc86757744c430c07473a54fa5321dd4a9536d
yzzhou <920115128@qq.com>
2017-05-22 21:24:57 +0800

    03_监听卡并且提示&监听拖动完成&显示网速

--- app/src/main/java/myapplication/mediaplayertest/utils/Utils.java
+++ app/src/main/java/myapplication/mediaplayertest/utils/Utils.java
@@ -1,6 +1,9 @@
 package myapplication.mediaplayertest.utils;
 
 
+import android.content.Context;
+import android.net.TrafficStats;
+
 import java.util.Formatter;
 import java.util.Locale;
 
@@ -11,6 +14,8 @@ import java.util.Locale;
 public class Utils {
     private StringBuilder mFormatBuilder;
     private Formatter mFormatter;
+    private long lastTotalRxBytes = 0;
+    private long lastTimeStamp = 0;
 
     public Utils() {
         // 转换成字符串的时间
@@ -51,4 +56,13 @@ public class Utils {
         }
             return isNetUri;
         }
+    public String getNetSpeed(Context context) {
+        long nowTotalRxBytes = TrafficStats.getUidRxBytes(context.getApplicationInfo().uid)==TrafficStats.UNSUPPORTED ? 0 :(TrafficStats.getTotalRxBytes()/1024);//转为KB;
+        long nowTimeStamp = System.currentTimeMillis();
+        long speed = ((nowTotalRxBytes - lastTotalRxBytes) * 1000 / (nowTimeStamp - lastTimeStamp));//毫秒转换
+        lastTimeStamp = nowTimeStamp;
+        lastTotalRxBytes = nowTotalRxBytes;
+        String msg  = String.valueOf(speed) + " kb/s";
+        return msg;
+    }
     }
